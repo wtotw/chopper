e5da8dc93fb52212d006a9f5d56a3e2e874ccba0
wtotw <s135061@gmail.com>
2020-08-09 02:18:53 +0900

    戻し

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,7 @@ const gpOptions =
     ? {
         router: {
           base: '/chopper/'
-        },
-        // build :{
-        //   publicPath: '/static/',
-        //   extend(config, ctx) {}
-        // }
+        }
       }
     : {};
 
@@ -23,7 +19,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: 'server',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
